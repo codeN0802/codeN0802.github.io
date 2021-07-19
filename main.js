d211bbf3b875c19132d58fc6876592320492d64c
huunghi <nghinguyen08022000@gmail.com>
2021-07-20 00:26:27 +0700

    fixed time

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
 let inputvalue;
-var inputele = document.querySelector('input[type="date"]');
+var inputele = document.querySelector('input[type="datetime-local"]');
 console.log(inputele);
-inputele.onchange = function(e){
+inputele.onchange = function (e) {
+
   inputvalue = e.target.value;
+
   (function () {
     const second = 1000,
-          minute = second * 60,
-          hour = minute * 60,
-          day = hour * 24;
-          
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
     let birthday = inputvalue,
-        countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {    
-  
-          let now = new Date().getTime(),
-              distance = countDown - now;
-  
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-  
-          //do something later when date is reached
-          if (distance < 0) {
-            let headline = document.getElementById("headline"),
-                countdown = document.getElementById("countdown"),
-                content = document.getElementById("content");
-  
-            headline.innerText = "It's my birthday!";
-            countdown.style.display = "none";
-            content.style.display = "block";
-  
-            clearInterval(x);
-          }
-          //seconds
-        }, 0)
-    }());
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function () {
+
+        let now = new Date().getTime(),
+          distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+            countdown = document.getElementById("countdown"),
+            content = document.getElementById("content");
+
+          headline.innerText = "It's my birthday!";
+          countdown.style.display = "none";
+          content.style.display = "block";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
 
 };
 
